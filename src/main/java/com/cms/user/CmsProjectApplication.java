cc5b53954e0b480a1a5b85765d2bfcc50adbdc08
soheeKang <thgml352@gmail.com>
2022-10-04 23:40:57 +0900

    Initial commit

---
+++ src/main/java/com/cms/user/CmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cms.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsProjectApplication.class, args);
+    }
+
+}
