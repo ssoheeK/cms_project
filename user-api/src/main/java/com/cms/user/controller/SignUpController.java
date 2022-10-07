42b7ee80cdeb30df0df4a0a3aaac6eacba2655ff
soheeKang <thgml352@gmail.com>
2022-10-07 11:13:19 +0900

    feat: [007-SCRATCH-USER] test 스크래치 파일 및 api 컨벤션 수정

--- user-api/src/main/java/com/cms/user/controller/SignUpController.java
+++ user-api/src/main/java/com/cms/user/controller/SignUpController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/signup")
+@RequestMapping("/signUp")
 @RequiredArgsConstructor
 public class SignUpController {
 
