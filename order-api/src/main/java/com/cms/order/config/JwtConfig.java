4a62d20b682d386c69172e623ba935b873d3302b
soheeKang <thgml352@gmail.com>
2022-10-07 17:45:21 +0900

    feat: [008-INIT-ORDER-API] order-api 초기 세팅

---
+++ order-api/src/main/java/com/cms/order/config/JwtConfig.java
@@ -0,0 +1,13 @@
+package com.cms.order.config;
+
+import com.sohee.domain.config.JwtAuthenticationProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+    @Bean
+    public JwtAuthenticationProvider jwtAuthenticationProvider() {
+        return new JwtAuthenticationProvider();
+    }
+}
