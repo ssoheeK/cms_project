22115bc92a4b07b19e18d24252f5c364cdbba133
soheeKang <thgml352@gmail.com>
2022-10-11 00:27:40 +0900

    feat: [015-UPDATE-CART] 장바구니 수정
    장바구니 조회 로직 재사용을 통한 코드 생산성 향상

--- order-api/src/main/java/com/cms/order/application/CartApplication.java
+++ order-api/src/main/java/com/cms/order/application/CartApplication.java
@@ -40,6 +40,12 @@ public class CartApplication {
         return cartService.addCart(customerId, form);
     }
 
+    public Cart updateCart(Long customerId, Cart cart) {
+        cartService.putCart(customerId, cart);
+        return getCart(customerId);
+    }
+
+
     public Cart getCart(Long customerId) {
         Cart cart = refreshCart(cartService.getCart(customerId));
         Cart returnCart = new Cart();
